f59c8e9f5c57177473c0f1bb8878e1958b0e2600
johnfedoruk <johnny@johnfedoruk.ca>
2018-07-15 16:28:32 -0500

    Implemented ErrorComponent's basic logic and view

--- src/app/pages/error/error.component.ts
+++ src/app/pages/error/error.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
-  selector: 'app-error',
-  templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css']
+    selector: 'app-error',
+    templateUrl: './error.component.html',
+    styleUrls: ['./error.component.css']
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+    public status_code: number;
 
-  ngOnInit() {
-  }
+    constructor(private activatedRoute: ActivatedRoute) { }
+
+    public ngOnInit(): void {
+        this.activatedRoute.params.subscribe(
+            (params: Params) => {
+                this.parseStatusCode(params);
+            }
+        );
+    }
+
+    private parseStatusCode(params: Params): void {
+        let status_code: number;
+        try {
+            status_code = +params.status_code;
+            if (isNaN(status_code)) {
+                throw new Error(`Status code ${params.status_code} is NaN`);
+            }
+        } catch (e) {
+            console.error(e);
+            status_code = 421;
+        } finally {
+            this.status_code = status_code;
+        }
+    }
 
 }
