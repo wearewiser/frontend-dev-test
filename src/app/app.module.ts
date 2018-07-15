adc85194301c21b31f41e1e18cbc19790b46726e
johnfedoruk <johnny@johnfedoruk.ca>
2018-07-15 15:36:54 -0500

    Created a HomeComponent to render the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './src/app/pages/home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
