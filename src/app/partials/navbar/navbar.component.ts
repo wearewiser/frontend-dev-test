494f26b90d5ed01b3918abd2c2eb71064bb263db
johnfedoruk <johnny@johnfedoruk.ca>
2018-07-15 23:53:14 -0500

    NavbarComponent implemented to render appropriate links when or when not authenticated

--- src/app/partials/navbar/navbar.component.ts
+++ src/app/partials/navbar/navbar.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from 'stub';
+import { Subscription } from 'rxjs/Subscription';
+import { Account } from 'common';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    private account: Account;
+    private subscriptions: Subscription[] = [];
 
-  ngOnInit() {
-  }
+    constructor(private authService: AuthService) { }
+
+    public get name(): string {
+        if (!this.authenticated) {
+            return '';
+        }
+        return this.account.username;
+    }
+
+    public get authenticated(): boolean {
+        return this.account !== undefined;
+    }
+
+    public ngOnInit(): void {
+        this.subscriptions.push(
+            this.authService.profile$.subscribe(
+                (profile: Account) => this.account = profile
+            )
+        );
+    }
+
+    public ngOnDestroy(): void {
+        this.subscriptions.forEach(sub => sub.unsubscribe());
+    }
 
 }
