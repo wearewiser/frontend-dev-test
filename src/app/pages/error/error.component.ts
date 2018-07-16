98726b83d130c38cd7183b7a7e4e87ecbce92cfb
johnfedoruk <johnny@johnfedoruk.ca>
2018-07-15 21:46:31 -0500

    Zombie subscription cleanup

--- src/app/pages/error/error.component.ts
+++ src/app/pages/error/error.component.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'app-error',
     templateUrl: './error.component.html',
     styleUrls: ['./error.component.css']
 })
-export class ErrorComponent implements OnInit {
+export class ErrorComponent implements OnInit, OnDestroy {
 
     public status_code: number;
+    private subscriptions: Subscription[] = [];
 
     constructor(private activatedRoute: ActivatedRoute) { }
 
     public ngOnInit(): void {
-        this.activatedRoute.params.subscribe(
-            (params: Params) => {
-                this.parseStatusCode(params);
-            }
+        this.subscriptions.push(
+            this.activatedRoute.params.subscribe(
+                (params: Params) => {
+                    this.parseStatusCode(params);
+                }
+            )
         );
+    }
+
+    public ngOnDestroy(): void {
+        this.subscriptions.forEach(sub => sub.unsubscribe());
     }
 
     private parseStatusCode(params: Params): void {
