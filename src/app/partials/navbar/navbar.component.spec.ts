6def499d3ad1db23e876fe3c8b92170638d31669
johnfedoruk <johnny@johnfedoruk.ca>
2018-07-16 00:21:25 -0500

    Added additional tests to NavbarComponent

--- src/app/partials/navbar/navbar.component.spec.ts
+++ src/app/partials/navbar/navbar.component.spec.ts
@@ -24,4 +24,27 @@ describe('NavbarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('authenticated',()=>{
+    it('should return false if account is undefined',()=>{
+        component['account'] = undefined;
+        expect(component.authenticated).toBeFalsy();
+    });
+    it('should return true if account is not undefined',()=>{
+        component['account'] = {username:'test',password:'test',expires:0};
+        expect(component.authenticated).toBeTruthy();
+    });
+  });
+
+  describe('name',()=>{
+    it('should return an empty string if account is undefined',()=>{
+        component['account'] = undefined;
+        expect(component.name).toEqual('');
+    });
+    it('should return the account\'s username if account is not undefined',()=>{
+        const username: string = "testing";
+        component['account'] = {username,password:'test',expires:0};
+        expect(component.name).toEqual(username);
+    });
+  });
 });
